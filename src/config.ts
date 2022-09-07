571f18dd1bcf8a6990d1c212ce14ae8160c03181
Eric Quanz <2374654+ericorruption@users.noreply.github.com>
2022-09-07 23:02:47 +0200

    chore: update title and links

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Eric Quanz - Software engineer";
+export const SITE_DESCRIPTION = `Software engineer with a graphic design background. Over 10 years experience developing and designing websites and web applications.`;
