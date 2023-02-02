b61888e761183615ecb1bc379d85f6df89da5a0a
Eric Quanz <2374654+ericorruption@users.noreply.github.com>
2023-02-02 13:43:21 +0100

    fix: broken sitemap after astro upgrade

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,16 @@
-import rss from '@astrojs/rss';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 
-export const get = () =>
-	rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: import.meta.env.SITE,
-		items: import.meta.glob('./blog/**/*.md'),
-	});
+export async function get(context) {
+  const posts = await getCollection("blog");
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map((post) => ({
+      ...post.data,
+      link: `/blog/${post.slug}/`,
+    })),
+  });
+}
